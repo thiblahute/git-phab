e2b40e660a3cf114668d42b005c9820b12953a12
Thibault Saunier <tsaunier@gnome.org>
2016-04-06 09:24:27 +0200

    Use the pre-commit project to do the linting
    Differential Revision: https://phabricator.freedesktop.org/D890

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-import os
+import subprocess
+import sys
 
 from setuptools import setup
 
+setup_requires=['pre-commit']
 
 setup(
     name="git-phab",
-    version="1.0",
+    version="1.9.0",
     author="Xavier Claessens",
     author_email="xavier.claessens@collabora.com",
     description=("Git subcommand to integrate with phabricator"),
@@ -13,11 +15,16 @@ setup(
     keywords="phabricator tool git",
     url="http://packages.python.org/git-phab",
     long_description_markdown_filename='README.md',
-    setup_requires=['setuptools-markdown'],
+    setup_requires=setup_requires,
     classifiers=[
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU General Public License (GPL)"
     ],
-    install_requires=['GitPython', 'appdirs', 'argcomplete'],
+    install_requires=['GitPython>=2.0.0', 'appdirs', 'argcomplete', 'phabricator'],
     scripts=['git-phab'],
 )
+
+try:
+    subprocess.check_call(["pre-commit", "install"])
+except subprocess.CalledProcessError:
+    print("Could not install `pre-commit` hook")
